ef5837815b09c96246dd8eaef8db475bdc3b2075
Sadie L. Bartholomew <sadie.bartholomew@ncas.ac.uk>
2025-12-11 14:24:30 +0000

    Import module functionality via __init__ ready to remove cfplot.py

--- cfplot/__init__.py
+++ cfplot/__init__.py
@@ -9,10 +9,17 @@ __maintainer__ ="Sadie Bartholomew"
 __date__ = "28th April 2025"
 __version__ = "3.4.0"
 
+import os
 
-from .cfplot import *  # noqa: F403, F401
+import cartopy
+import cf
+from distutils.version import StrictVersion
+import matplotlib
 
+# Import module functionality -------------------------------------------
 # Imports to export functions: cfp.<module>.<function> -> cfp.<function>
+# either as intended going forward or to preserve existing API.
+# TODO review what should be available at module level.
 from .contour import con
 from .vector import vect
 from .stipple import stipple
@@ -20,6 +27,76 @@ from .trajectory import traj
 from .line import lineplot
 from .stream import stream
 
+from .calculate import (
+    calculate_levels,
+)
+from .colour import (
+    cbar,
+    _process_color_scales,
+)
+from .graphic import (
+    _which,
+    gclose,
+    gopen,
+    gpos,
+)
+from .mapping import (
+    _cscale_get_map,
+    _mapaxis,
+    _map_title,
+    _plot_map_axes,
+    _set_map,
+    axes_plot,
+    map_grid,
+)
+from .parameters import (
+    allvars_defaults,
+    axes,
+    cscale,
+    cscale1,
+    global_blockfill,
+    global_fill,
+    global_lines,
+    gset,
+    levs,
+    mapset,
+    plotvars,
+    plotvars_defaults,
+    pvars,
+    reset,
+    setvars,
+    setvars_defaults,
+    viridis,
+)
+from .utils import (
+    _bfill,
+    _bfill_ugrid,
+    _cf_data_assign,
+    _dim_titles,
+    _gvals,
+    _supscr,
+    _timeaxis,
+    add_cyclic,
+    cf_var_name,
+    cf_var_name_titles,
+    find_dim_names,
+    find_pos_in_array,
+    find_z,
+    fix_floats,
+    generate_titles,
+    irregular_window,
+    max_ndecs_data,
+    ndecs,
+    pcon,
+    polar_regular_grid,
+    regrid,
+    rgaxes,
+    stipple_points,
+    vloc,
+)
+
+
+# Process versions and display ------------------------------------------
 
 # Check for the minimum cf-python version
 cf_version_min = "3.17.0"
